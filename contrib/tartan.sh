b4aeeb013fcc43779e3291893d08b057c1c2fdea
Thomas Mühlbacher <tmuehlbacher@posteo.net>
2026-04-25 17:14:32 +0200

    trivial: add tartan build to CI

--- contrib/tartan.sh
+++ contrib/tartan.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/bin/scan-build-18 \
+exec /usr/bin/scan-build-18 \
     -load-plugin /usr/lib64/tartan/18.1/libtartan.so \
     -disable-checker core.CallAndMessage \
     -disable-checker core.NullDereference \
